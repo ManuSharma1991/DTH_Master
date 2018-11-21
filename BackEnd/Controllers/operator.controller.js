193dcd87dff2b7938d0193bbb5bd2bdf42e96c82
Viswanadha Manu Sharma <manu.viswanad@gmail.com>
2018-11-21 23:25:15 +0530

    Operator CRUD in backEnd with search done.

--- BackEnd/Controllers/operator.controller.js
+++ BackEnd/Controllers/operator.controller.js
@@ -51,43 +51,95 @@ exports.findOne = (req, res) => {
     console.log('line 51 ' + req.params.operatorId)
 
     Operator.findOne({
-        operatorID: req.params.operatorId
-    }, function (err, operator) {
-        if (err) {
-            return res.status(200).send(err);
-        }
-        return res.status(200).send(operator);
-    })
-
-    // Operator.findOne({
-    //         'operator.operatorID': req.params.operatorId
-    //     })
-    //     .then(operator => {
-    //         if (!operator) {
-    //             return res.status(404).send({
-    //                 message: "Operator not found with id ==================== " + req.params.operatorId
-    //             });
-    //         }
-    //         res.send(operator);
-    //     }).catch(err => {
-    //         console.log(err);
-    //         if (err.kind === 'ObjectId') {
-    //             return res.status(404).send({
-    //                 message: "Operator not found with id " + req.params.operatorId
-    //             });
-    //         }
-    //         return res.status(500).send({
-    //             message: "Error retrieving operator with id " + req.params.operatorId
-    //         });
-    //     });
+            operatorID: req.params.operatorId
+        })
+        .then(operator => {
+            if (!operator) {
+                return res.status(404).send({
+                    message: "Operator not found with id" + req.params.operatorId
+                });
+            }
+            res.send(operator);
+        }).catch(err => {
+            console.log(err);
+            if (err.kind === 'ObjectId') {
+                return res.status(404).send({
+                    message: "Operator not found with id " + req.params.operatorId
+                });
+            }
+            return res.status(500).send({
+                message: "Error retrieving operator with id " + req.params.operatorId
+            });
+        });
 };
 
 // Update a operator identified by the operatorId in the request
 exports.update = (req, res) => {
 
+    // Find operator and update it with the request body
+    Operator.findOneAndUpdate({
+            operatorID: req.params.operatorId
+        }, {
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+
+            emailid: req.body.emailid,
+            phonenumber: req.body.phonenumber,
+
+            shiftstarttime: req.body.shiftstarttime,
+            shiftendtime: req.body.shiftendtime,
+
+            startmeridian: req.body.startmeridian,
+            endmeridian: req.body.endmeridian,
+
+            maximumnumberofcustomers: req.body.maximumnumberofcustomers,
+
+            operatorID: req.body.operatorID,
+            password: req.body.password || 'Operator@1234'
+        }, {
+            new: true
+        })
+        .then(operator => {
+            if (!operator) {
+                return res.status(404).send({
+                    message: "Operator not found with id " + req.params.operatorId
+                });
+            }
+            res.send(operator);
+        }).catch(err => {
+            if (err.kind === 'ObjectId') {
+                return res.status(404).send({
+                    message: "Operator not found with id " + req.params.operatorId
+                });
+            }
+            return res.status(500).send({
+                message: "Error updating operator with id " + req.params.operatorId
+            });
+        });
 };
 
 // Delete a operator with the specified operatorId in the request
 exports.delete = (req, res) => {
-
+    Operator.findOneAndDelete({
+            operatorID: req.params.operatorId
+        })
+        .then(operator => {
+            if (!operator) {
+                return res.status(404).send({
+                    message: "Operator not found with id " + req.params.operatorId
+                });
+            }
+            res.send({
+                message: "Operator deleted successfully!"
+            });
+        }).catch(err => {
+            if (err.kind === 'ObjectId' || err.name === 'NotFound') {
+                return res.status(404).send({
+                    message: "Operator not found with id " + req.params.operatorId
+                });
+            }
+            return res.status(500).send({
+                message: "Could not delete operator with id " + req.params.operatorId
+            });
+        });
 };
